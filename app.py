8581dcbfe7154e221dd668d7da843fe76dc8d799
johnkwandikwa <johne160408@gmail.com>
2020-10-20 10:49:15 +0300

    Changes on js models

--- app.py
+++ app.py
@@ -9,6 +9,12 @@ def index():
 
     return render_template('index.html')
 
+@app.route('/movies')
+def movies():
+    return render_template('movies.html')
+
+
+
 
 if __name__ == '__main__':
     app.run()
