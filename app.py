251ea85aeb02c4e17fad30660eb9940998fada8f
johnkwandikwa <johne160408@gmail.com>
2020-10-22 10:44:39 +0300

    The register module on login/register modal

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
 
-from flask import Flask, render_template
+from flask import Flask, render_template,request,redirect,url_for
+from authenicationcontrol import Authentication
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/',methods=['POST','GET'])
 def index():
+    control =Authentication()
+    if request.method == 'POST':
+        register_status=control.registerNewUser(request)
+        if register_status ==1:
+            return render_template('index.html')
+        else:
+            return redirect(url_for(index))
 
     return render_template('index.html')
 
